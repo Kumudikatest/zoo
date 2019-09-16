ba1f6acb032555c21641413584dbb934991c513b
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-09-16 16:52:59 +0530

    add s3

--- zoo/uploader.js
+++ zoo/uploader.js
@@ -1,11 +1,26 @@
-exports.handler = function(event, context, callback) {
+let AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
+exports.handler = function (event, context, callback) {
+    let img = Buffer.from(event.body,"base64");
     console.log(event);
-    callback(null, {
-  "isBase64Encoded": true,
-  "statusCode": 200,
-  "headers": {
-    "headerName": "headerValue"
-  },
-  "body": "..."
-});
+    s3.putObject({
+        "Body": img,
+        "Bucket": "zoobucket",
+        "Key": Date.now(),
+        "ACL": "public-read"
+    })
+        .promise()
+        .then(data => {
+            callback(null, {
+                "isBase64Encoded": true,
+                "statusCode": 200,
+                "body": "Success"
+            });
+        })
+        .catch(err => {
+            console.log(err, err.stack);
+        });
+
+
 }
